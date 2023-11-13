94c228dd52961b8d27c11287d3fdc7b634781fde
Darius Kianersi <radius@gammawks16.umiacs.umd.edu>
2023-11-13 15:01:15 -0500

    rerun with more epochs and lower lr; improve metrics

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torchvision.transforms import v2
 from torch.utils.data import DataLoader
+
 import einops
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -82,10 +83,11 @@ def get_steering_data():
 @dataclass
 class SteeringTrainingArgs():
     batch_size: int = 256
-    num_epochs: int = 20
-    learning_rate: float = 0.0001
+    num_epochs: int = 1000
+    learning_rate: float = 0.001
     optimizer = torch.optim.Adam
     loss_fn = nn.MSELoss()
+    logger: str = "results/results.log"
 
 class SteeringTrainer():
     def __init__(self, args: SteeringTrainingArgs):
@@ -97,33 +99,57 @@ class SteeringTrainer():
         self.steering_trainset, self.steering_testset = get_steering_data()
         self.steering_trainloader = DataLoader(self.steering_trainset, batch_size=args.batch_size, shuffle=True)
         self.steering_testloader = DataLoader(self.steering_testset, batch_size=args.batch_size, shuffle=False)
-        self.loss_list = []
-        self.accuracy_list = []
+        # self.loss_list = []
+        # self.accuracy_list = []
+        self.epsilons = [0.1, 0.2, 0.5, 1, 2, 5]
+        self.num_train_batches = int(len(self.steering_trainset) / self.batch_size)
+        self.num_test_batches = int(len(self.steering_testset) / self.batch_size)
+        self.logger = args.logger
 
     def train(self):
-        for i in tqdm(range(self.num_epochs)):
+        for i in range(self.num_epochs):
             self.model.train()
+            train_epoch_loss = 0
+            train_acc = [0 for _ in range(len(self.epsilons))]
+            test_acc = [0 for _ in range(len(self.epsilons))]
             for imgs, labels in tqdm(self.steering_trainloader):
                 imgs = imgs.to(device)
                 labels = labels.to(device)
                 Y = self.model(imgs).squeeze()
                 loss = self.loss_fn(Y, labels)
                 loss.backward()
+                train_epoch_loss += loss.item()
+                for idx, eps in enumerate(self.epsilons):
+                    train_acc[idx] += ((Y - labels).abs() < eps).sum()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
-                self.loss_list.append(loss)
-                print(f"\nEpoch {i} Training Loss: {loss}\n")
+                # self.loss_list.append(loss.item())
             self.model.eval()
-            eval_loss = 0
+            test_epoch_loss = 0
             for imgs, labels in tqdm(self.steering_testloader):
                 imgs = imgs.to(device)
                 labels = labels.to(device)
                 with torch.inference_mode():
                     Y = self.model(imgs).squeeze()
-                    eval_loss += self.loss_fn(Y, labels).sum()
-            avg_eval_loss = eval_loss / len(self.steering_testset)
-            self.accuracy_list.append(avg_eval_loss)
-            print(f"Evaluation loss at epoch {i}: {avg_eval_loss}")
+                    test_epoch_loss += self.loss_fn(Y, labels).sum()
+                    for idx, eps in enumerate(self.epsilons):
+                        test_acc[idx] += ((Y - labels).abs() < eps).sum()
+            train_acc = [acc / len(self.steering_trainset) for acc in train_acc]
+            test_acc = [acc / len(self.steering_testset) for acc in test_acc]
+            avg_eval_loss = test_epoch_loss / len(self.steering_testset)
+            # self.accuracy_list.append(avg_eval_loss)
+            # print(f"Evaluation loss at epoch {i}: {avg_eval_loss}")
+            info = {
+                "epoch": f"[{i}/{self.num_epochs}]",
+                "training loss": train_epoch_loss/self.num_train_batches,
+                "train accuracy": torch.mean(torch.stack(train_acc), dtype=torch.float32).item(),
+                "test loss": test_epoch_loss/self.num_test_batches,
+                "test accuracy": torch.mean(torch.stack(test_acc), dtype=torch.float32).item(),
+            }
+            with open(self.logger, "a") as f:
+                f.write(", ".join([f"{k}: {v}" for k, v in info.items()]))
+                f.write("\n")
+            print(", ".join([f"{k}: {v}" for k, v in info.items()]))
         path = "./results/model.pth"
         print(f"Saving model to {path}.")
         torch.save(self.model.state_dict(), "./results/model.pth")
@@ -132,11 +158,24 @@ class SteeringTrainer():
 # trainer = SteeringTrainer(args)
 # trainer.train()
 
-model = SteeringModel()
-model.load_state_dict(torch.load("./results/model.pth"))
+# model = SteeringModel().to(device)
+model = net_nvidia_pytorch().to(device)
+model.load_state_dict(torch.load("../Multi_Perturbation_Robustness/results/train_results/trainB_/model-final_0.pth"))
+# model.load_state_dict(torch.load("./results/model.pth"))
 steering_trainset, steering_testset = get_steering_data()
-print(model(einops.rearrange(steering_trainset[3269][0], "c h w -> 1 c h w")), steering_trainset[3269][1])
-# print(steering_trainset[3269])
-# for i in range(len(steering_trainset)):
-#     print(steering_trainset[i][1])
-# print(steering_trainset[0][0], steering_trainset[0][0].shape)
+# print(model(einops.rearrange(steering_testset[0][0].to(device), "c h w -> 1 c h w")), steering_testset[0][1])
+steering_testloader  = DataLoader(steering_testset, batch_size=256, shuffle=False)
+total_loss = torch.zeros(1).to(device)
+eps = 0.5
+acc = 0
+for imgs, labels in steering_testloader:
+    imgs = imgs.to(device)
+    labels = labels.to(device)
+    # out = model(imgs).squeeze()
+    out = model(imgs)[0].squeeze()
+    # print(pred)
+    acc += ((out - labels).abs() < eps).sum()
+    # total_loss += F.mse_loss(out, labels).sum()
+
+# print(f"MSE: {total_loss.item()}, {total_loss.item()/len(steering_testset)}")
+print(f"Accuracy: {acc / len(steering_testset)}")
